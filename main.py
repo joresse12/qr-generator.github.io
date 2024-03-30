cc1baeb2c0f5f0cbcdb6c9e27e7122923ac6c147
Joresse Zekeng <inf4016@hs-worms.de>
2024-03-30 18:59:26 +0100

    bug fix

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
-from PyQt6.QtWidgets  import QApplication, QMainWindow
-from qrCodeUI import  Ui_MainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt6.QtGui import QPixmap
+from qrCodeUI import Ui_MainWindow
+import qrcode
 
 
 class MainWindow(QMainWindow):
@@ -9,8 +11,38 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        
+        self.ui.generate_btn.clicked.connect(self.generate_qr)
 
+    def generate_qr(self):
+        
+        wifi_password = self.passwd_line.text()
+        wifi_ssid = self.lineEdit.text()
+        
+        wifi_security = "WPA"  # Sécurité WiFi, peut être WEP, WPA, ou sans fil
 
+        # Création de la chaîne de configuration WiFi pour le QR code
+        wifi_config = f"WIFI:T:{wifi_security};S:{wifi_ssid};P:{wifi_password};;"
+
+        # Création du QR code
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(wifi_config)
+        qr.make(fit=True)
+
+        # Génération de l'image du QR code
+        img = qr.make_image(fill_color="black", back_color="white")
+
+        pixmap = QPixmap.fromImage(img)
+        # Afficher le QR code dans votre interface graphique (vous devez ajouter un QLabel à votre fichier .ui pour afficher l'image)
+        self.qr_place.setPixmap(pixmap)
+        self.qr_place.setScaledContents(True)
+
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = MainWindow()

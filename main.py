2e7c64c11992890591d26baf0f17174e324e422e
Joresse Zekeng <inf4016@hs-worms.de>
2024-03-30 18:59:12 +0100

    update

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
-import qrcode
+import sys
+from PyQt6.QtWidgets  import QApplication, QMainWindow
+from qrCodeUI import  Ui_MainWindow
 
-# # img = qrcode.make("EvMa23aMTi")
-# # img.save("myQrCode.png")
 
-# Informations du réseau Wi-Fi
-wifi_ssid = "W701V-9"
-wifi_password = "EvMa23aMTi"
-wifi_security = "WPA"  # Sécurité WiFi, peut être WEP, WPA, ou sans fil
+class MainWindow(QMainWindow):
 
-# Création de la chaîne de configuration WiFi pour le QR code
-wifi_config = f"WIFI:T:{wifi_security};S:{wifi_ssid};P:{wifi_password};;"
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-# create the qr-code
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data(wifi_config)
-qr.make(fit=True)
 
-# generate Qr code image
-img = qr.make_image(fill_color="black", back_color="white")
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec())
 
-# image saving
-img.save("myWifiQRCode.png")
+# # # img = qrcode.make("EvMa23aMTi")
+# # # img.save("myQrCode.png")
+
+# # Informations du réseau Wi-Fi
+# wifi_ssid = "W701V-9"
+# wifi_password = "EvMa23aMTi"
+# wifi_security = "WPA"  # Sécurité WiFi, peut être WEP, WPA, ou sans fil
+
+# # Création de la chaîne de configuration WiFi pour le QR code
+# wifi_config = f"WIFI:T:{wifi_security};S:{wifi_ssid};P:{wifi_password};;"
+
+# # create the qr-code
+# qr = qrcode.QRCode(
+#     version=1,
+#     error_correction=qrcode.constants.ERROR_CORRECT_L,
+#     box_size=10,
+#     border=4,
+# )
+# qr.add_data(wifi_config)
+# qr.make(fit=True)
+
+# # generate Qr code image
+# img = qr.make_image(fill_color="black", back_color="white")
+
+# # image saving
+# img.save("myWifiQRCode.png")
